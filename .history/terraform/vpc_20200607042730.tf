9b2a4a7e7a343636727093b5a44bae978ba3be7c
Shershah <ssheru@gmail.com>
2020-06-07 07:21:41 +0100

    included history to gitignore list

--- .history/terraform/vpc_20200607042730.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "Assignment_vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
-
-  tags = {
-    Name = "BPDTS_vpc"
-  }
-}
--- .history/terraform/vpc_20200607070651.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "Assignment_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "dedicated"
-
-  tags = {
-    Name = "BPDTS_vpc"
-  }
-}
