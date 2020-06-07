f48d280843c3263e9b46def9c1471456e657609e
Shershah <ssheru@gmail.com>
2020-06-07 13:03:49 +0100

    chnaged from ubuntu to amazon

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "${var.region}"
 }
 resource "aws_instance" "testapp-server" {
-  ami           = "ami-003634241a8fcdec0"
+  ami           = "ami-0e34e7b9ca0ace12d"
   instance_type = "${var.instance_type}"
   key_name      = "${var.key_name}"
   user_data     = "${file("installations.sh")}"
--- terraform/installations.sh
+++ terraform/installations.sh
@@ -1,9 +1,10 @@
-     #! bin/bash
-     sudo apt update
-     sudo apt install git  apt python3.6 python3-pip -y
-     git clone https://github.com/ssheru/assignment.git
-     cd assignment/test-app/
-     pip3 install -r requirements.txt
-     python3 app.py
+       #!  bin/bash
+      sudo yum clean all
+      sudo yum update -y
+      sudo yum install git python36 python36-devel python36-pip -y
+      git clone https://github.com/ssheru/assignment.git
+      cd test-app/
+      pip-3.6 install -r requirements.txt
+      python3 app.py
 
 
