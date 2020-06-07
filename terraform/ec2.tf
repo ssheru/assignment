4d3155bac89a1d83aa9ccbf10c4972455d9ddf2f
Shershah <ssheru@gmail.com>
2020-06-07 14:23:40 +0100

    updates

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "${var.region}"
 }
 resource "aws_instance" "testapp-server" {
-  ami           = "ami-0e34e7b9ca0ace12d"
+  ami           = "ami-086b16d6badeb5716"
   instance_type = "${var.instance_type}"
   key_name      = "${var.key_name}"
   user_data     = "${file("installations.sh")}"
--- terraform/installations.sh
+++ terraform/installations.sh
@@ -1,10 +1,10 @@
-       #!  bin/bash
+       #!bin/bash
       sudo yum clean all
       sudo yum update -y
       sudo yum install git python36 python36-devel python36-pip -y
       git clone https://github.com/ssheru/assignment.git
-      cd test-app/
-      pip-3.6 install -r requirements.txt
+      cd /assignment/test-app/
+      sudo pip-3.6 install -r requirements.txt
       python3 app.py
 
 
