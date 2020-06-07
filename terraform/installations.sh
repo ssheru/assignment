c39af2639f8b475630895496a5342470208e57d0
Shershah <ssheru@gmail.com>
2020-06-07 07:50:37 +0100

    included packer details

--- terraform/installations.sh
+++ terraform/installations.sh
@@ -14,7 +14,7 @@
      rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
      yum install jenkins -y
      service jenkins start #systemctl start jenkins
-     service jenkins enable #systemctl enable jenkins
+     checkconfig jenkins on #systemctl enable jenkins
 
 ###***Packer Installation***###
      wget https://releases.hashicorp.com/packer/1.5.6/packer_1.5.6_linux_amd64.zip
